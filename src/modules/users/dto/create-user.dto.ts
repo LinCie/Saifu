c3c6261d71c999e0ca92f9197e4ea91a07795b4a
LinCie <aldiofernanda@protonmail.com>
2024-07-14 01:36:33 +0700

    populate DTOs and validate

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(8, 20)
+  @Matches(/^\S*$/, {
+    message: 'Username should not contain spaces',
+  })
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}/, {
+    message:
+      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+  })
+  password: string;
+}
