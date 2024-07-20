dd5ccb1ada694ebb1adce0ad00fc7ac4ea6363b3
LinCie <aldiofernanda@protonmail.com>
2024-07-20 13:55:10 +0700

    added validate user functionality

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async validateUser(createUserDto: CreateUserDto) {
+    const user = await this.usersService.findOne(createUserDto.username);
+    const isPasswordCorrect = await bcrypt.compare(
+      createUserDto.password,
+      user.hash,
+    );
+
+    if (isPasswordCorrect) {
+      return user;
+    }
+    return null;
+  }
+}
