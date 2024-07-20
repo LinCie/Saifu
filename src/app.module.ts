131ce84a839bd4f5cbc5ed6f5782460dff09c536
LinCie <aldiofernanda@protonmail.com>
2024-07-20 13:09:14 +0700

    added auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MikroOrmModule.forRoot(), UsersModule],
+  imports: [ConfigModule.forRoot(), MikroOrmModule.forRoot(), UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
