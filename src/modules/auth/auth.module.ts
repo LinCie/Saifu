978709e7c12cefd1f0a8e1b0d230a2e84344aaa3
LinCie <aldiofernanda@protonmail.com>
2024-07-22 23:59:31 +0700

    import passport necessity modules

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [UsersModule],
+  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule],
 })
 export class AuthModule {}
