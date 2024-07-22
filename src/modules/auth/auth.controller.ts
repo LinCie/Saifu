c0c0583f70933b4622a8ed09773556f14fcaee1c
LinCie <aldiofernanda@protonmail.com>
2024-07-22 23:59:46 +0700

    added signin route

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './strategy/local-auth.guard';
+import { RequestWithUser } from './interface/RequestWithUser';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('signin')
+  @UseGuards(LocalAuthGuard)
+  async signIn(@Request() req: RequestWithUser) {
+    return req.user;
+  }
+}
