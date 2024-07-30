61dd6fb365501e6290c621b18aa276951a811ca6
LinCie <aldiofernanda@protonmail.com>
2024-07-30 22:38:59 +0700

    added api versioning

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards, Version } from '@nestjs/common';
 import { LocalAuthGuard } from './strategy/local-auth.guard';
 import { RequestWithUser } from './interface/RequestWithUser';
 
 @Controller('auth')
 export class AuthController {
   @Post('signin')
+  @Version('1')
   @UseGuards(LocalAuthGuard)
   async signIn(@Request() req: RequestWithUser) {
     return req.user;
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Version,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,26 +17,31 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @Version('1')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @Version('1')
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':username')
+  @Version('1')
   findOne(@Param('username') username: string) {
     return this.usersService.findOne(username);
   }
 
   @Patch(':id')
+  @Version('1')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @Version('1')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
