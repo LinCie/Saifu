5680ae8e9060373f065528267597f3f6261de345
LinCie <aldiofernanda@protonmail.com>
2024-07-31 00:19:20 +0700

    added refresh strategy

---
+++ src/modules/auth/strategy/jwt-refresh.strategy.ts
@@ -0,0 +1,27 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '@/modules/users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromBodyField('refresh_token'),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { sub: string; username: string }) {
+    const user = await this.usersService.findOne(payload.sub);
+    if (user) {
+      return user;
+    }
+    return null;
+  }
+}
